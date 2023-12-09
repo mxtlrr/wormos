9efcd5272d3853bbddd4e092aeec169f22adefaf
mxtlrr <ruson0914@gmail.com>
2023-12-08 20:32:49 -0600

    no more warnings <3

--- src/kernel/libc/stdio.c
+++ src/kernel/libc/stdio.c
@@ -131,8 +131,7 @@ void dc_putc(char c){
 }
 
 void dc_puts(char* fmt){
-	for(int i = 0; i < strlen(fmt); i++)
-		dc_putc(fmt[i]);
+	for(size_t i = 0; i < strlen(fmt); i++) dc_putc(fmt[i]);
 }
 void dc_printf(char* fmt, ...){
 	va_list ap;
--- src/kernel/mem/mmap.c
+++ src/kernel/mem/mmap.c
@@ -1,10 +1,9 @@
 #include "mem/mmap.h"
 
 int get_mem_available(multiboot_info_t* mbd){
-  multiboot_uint32_t entry_no = 0;
   int length_raw = 0;
 
-	for(int i = 0; i < mbd->mmap_length; i += sizeof(multiboot_memory_map_t)){
+	for(multiboot_uint32_t i = 0; i < mbd->mmap_length; i += sizeof(multiboot_memory_map_t)){
 		multiboot_memory_map_t* mmmt = (multiboot_memory_map_t*)(mbd->mmap_addr + i);
 		if(mmmt->type == MMAP_AVAILABLE) length_raw += mmmt->len_low;
 	}
@@ -16,7 +15,7 @@ int get_mem_available(multiboot_info_t* mbd){
 mmap_entry_t entries[BLOCKS];
 void add_entries(multiboot_info_t* mbd){
   int _ent = 0;
-  for(int i = 0; i < mbd->mmap_length; i += sizeof(multiboot_memory_map_t)){
+  for(multiboot_uint32_t i = 0; i < mbd->mmap_length; i += sizeof(multiboot_memory_map_t)){
 		multiboot_memory_map_t* mmmt = (multiboot_memory_map_t*)(mbd->mmap_addr + i);
     mmap_entry_t mmap_n = {
       .availability = mmmt->type,
